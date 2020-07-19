113d0622693f16f4ac2e9f8e10e42477ee160dda
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-19 01:04:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 def reverse_each_word(phrase)
   list_phrase = phrase.split(' ')
-  #binding.pry
   reversed_phrase = list_phrase.each do |word|
     word.reverse!
-    return reversed_phrase.join(" ")
+    return reversed_phrase
   end
 end
